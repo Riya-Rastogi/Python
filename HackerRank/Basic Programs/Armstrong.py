d1ef9e4873bb906b0141d4de537cecf34089038b
Riya-Rastogi <riya.rastogi_cs20@gla.ac.in>
2022-06-28 20:14:50 +0530

    Basic program

---
+++ HackerRank/Basic Programs/Armstrong.py
@@ -0,0 +1,13 @@
+number1 = n
+temp = n
+while n!=0:
+    n = (n//10)
+    i=i+1;
+while number1!=0:
+    n=number1%10
+    result=result+pow(n,i)
+    number1=number1//10
+if temp==result:
+    print("number is armstrong")
+else:
+    print("number is not armstrong") 
